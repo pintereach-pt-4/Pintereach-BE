cfe69d450f656d5cd82ae0c47aa5e7889e9178b1
Seun-O <12373359+Seun-O@users.noreply.github.com>
2019-07-11 12:28:24 -0400

    Created test db helper method
    Co-authored-by: rodbromberg <rod.bromberg@yahoo.com>

--- models/boardModel/index.js
+++ models/boardModel/index.js
@@ -1,1 +1,13 @@
 const db = require("../../db");
+
+const getUsers = () => {
+  return db("boards");
+};
+
+// Function to Test Database Helper Methods
+const execute = async () => {
+  const data = await getUsers();
+  console.log(data);
+};
+
+execute();
