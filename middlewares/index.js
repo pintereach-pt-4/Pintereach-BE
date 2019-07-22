b3fc98ea47cd5f919227350a99da4deeda67e835
Rod <rod.bromberg@yahoo.com>
2019-07-21 22:38:44 -0400

    deleted files added comments

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,6 +3,10 @@ const bcrypt = require("bcryptjs");
 const { generateToken, authenticate } = require("../config");
 const db = require("../models/userModel");
 
+// takes in a user and pulls the users password
+// if no password provided returns 'password required'
+//  hash's user password and salts 2^12
+
 const hashPass = (req, res, next) => {
   let user = req.body;
   if (!user.password) {
@@ -19,12 +23,15 @@ const hashPass = (req, res, next) => {
 
 async function protectedRoute(req, res, next) {
   try {
+    // looks for a users token
     const token = await authenticate(req.headers.token);
+    // if no token returns a 401 error message
     if (!token) {
       return res
         .status(401)
         .json({ message: "No token provided. Please authenticate" });
     } else {
+      // if its found save it to be used later
       req.decoded = token;
       next();
     }
@@ -37,10 +44,13 @@ async function auth(req, res, next) {
   try {
     const body = req.body;
     const user = await db.getUserName(body.username);
+    // searches for a users name
     if (!user) {
       res.status(404).json({ message: "Enter a valid username and password!" });
     }
     if (user && bcrypt.compareSync(body.password, user.password)) {
+      // compares given password with db's password counters timing attacks constant time algorithms
+
       const token = await generateToken(user);
       const decodedToken = await authenticate(token);
       req.token = token;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const userRoutes = require("./users");
 const boardRoutes = require("./boards");
 const authRoutes = require("./auth");
 
+var a = 5;
+
 module.exports = {
   userRoutes,
   boardRoutes,
